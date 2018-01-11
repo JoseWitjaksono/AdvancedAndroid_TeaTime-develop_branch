4b292b3c55efb467d9342b7e7b0471b90a890599
Reauleux <reauleux.rx@gmail.com>
2018-01-12 06:44:38 +0700

    AddOrderSummaryActivity

--- app/src/androidTest/java/com/example/android/teatime/OrderSummaryActivityTest.java
+++ app/src/androidTest/java/com/example/android/teatime/OrderSummaryActivityTest.java
@@ -16,19 +16,6 @@
 
 package com.example.android.teatime;
 
-import static android.app.Instrumentation.ActivityResult;
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.intent.Intents.intended;
-import static android.support.test.espresso.intent.Intents.intending;
-import static android.support.test.espresso.intent.matcher.IntentMatchers.hasAction;
-import static android.support.test.espresso.intent.matcher.IntentMatchers.hasExtra;
-import static android.support.test.espresso.intent.matcher.IntentMatchers.isInternal;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
-
-import static org.hamcrest.core.AllOf.allOf;
-import static org.hamcrest.core.IsNot.not;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.support.test.espresso.intent.rule.IntentsTestRule;
@@ -39,18 +26,28 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.app.Instrumentation.ActivityResult;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.Intents.intending;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasAction;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasExtra;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.isInternal;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.core.AllOf.allOf;
+import static org.hamcrest.core.IsNot.not;
+
 @RunWith(AndroidJUnit4.class)
 public class OrderSummaryActivityTest {
 
     private static final String emailMessage = "I just ordered a delicious tea from TeaTime. Next time you are craving a tea, check them out!";
 
     /**
-     *
      * This test demonstrates Espresso Intents using the IntentsTestRule, a class that extends
      * ActivityTestRule. IntentsTestRule initializes Espresso-Intents before each test that is annotated
      * with @Test and releases it once the test is complete. The designated Activity
      * is also terminated after each test.
-     *
      */
 
     @Rule

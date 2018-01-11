1a306b3c3f5688f0d089f6fba1e3594f05455b02
Reauleux <reauleux.rx@gmail.com>
2018-01-12 06:48:22 +0700

    AddIdlingResourceMenuActivityTest

--- app/src/androidTest/java/com/example/android/teatime/IdlingResourceMenuActivityTest.java
+++ app/src/androidTest/java/com/example/android/teatime/IdlingResourceMenuActivityTest.java
@@ -17,12 +17,6 @@
 package com.example.android.teatime;
 
 
-import static android.support.test.espresso.Espresso.onData;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
-
-import static org.hamcrest.Matchers.anything;
-
 import android.support.test.espresso.Espresso;
 import android.support.test.espresso.IdlingResource;
 import android.support.test.rule.ActivityTestRule;
@@ -34,15 +28,20 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.Matchers.anything;
+
 /**
  * Usually Espresso syncs all view operations with the UI thread as well as AsyncTasks, but it can't
  * do so with custom resources (e.g. activity or service). For such cases, we can register the
  * custom resource and Espresso will wait for the resource to be idle before
  * executing a view operation.
- *
+ * <p>
  * In this example, we simulate an idling situation. This test is the same as the
  * MenuActivityScreenTest but with an Idling Resource to help with synchronization.
- *
+ * <p>
  * We added an idling period from when the user clicks on a GridView item
  * in MenuActivity to when corresponding order activity appears. This is to simulate potential
  * delay that could happen if this data were being retrieved from the web. Without registering the
@@ -58,7 +57,7 @@ public class IdlingResourceMenuActivityTest {
      * The ActivityTestRule is a rule provided by Android used for functional testing of a single
      * activity. The activity that will be tested, MenuActivity in this case, will be launched
      * before each test that's annotated with @Test and before methods annotated with @Before.
-     *
+     * <p>
      * The activity will be terminated after the test and methods annotated with @After are
      * complete. This rule allows you to directly access the activity during the test.
      */
